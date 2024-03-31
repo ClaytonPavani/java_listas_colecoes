6fd989e1735718a23280a66ff063fbbaf199b232
ClaytonPavani <claytonpavani@gmail.com>
2024-03-31 16:55:22 -0300

    Commit do conceitos, projeto, e aplicaçãoes de listas, coleções, utilizando interfaces

---
+++ src/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,79 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie("Lost", 2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
+        // Na linha abaixo, criamos um método construtor, já incluso o inicializador do nome:
+        // Esse valor do nome foi feito em um método da classe filme
+
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        // Vamos usar o método size para ver o tamanho da lista:
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        // Vamos usar o método get para pegar a posição 0 - primeiro da lista
+        // Usamos o get nome em seguida para pegar o nome da posição que pegamos anteriormente:
+        System.out.println("Primeiro Filme: " + listaDeFilmes.get(0).getNome());
+        // Vamos ver se podemos imprimir a lista completa - exibirá o endereço:
+        System.out.println(listaDeFilmes);
+        // Compilando método toString do método: após ter alterado o toString na classe "Filme.java"
+        System.out.println("toString do Filme: " + listaDeFilmes.get(0).toString());
+
+
+
+
+
+    }
+}
